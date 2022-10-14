a63977dbbb7d7f428c2fc2e26d95dd506f2efaea
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-14 11:55:26 +0200

    FEAT: added class and style for body and pokeDiv. FIX: the use of an unnecessary fecth, combining both

--- pokeproyect.js
+++ pokeproyect.js
@@ -1,24 +1,20 @@
-const print = (pokemons) => {
+const print = (pokeData) => {
 
-    for (pokemonNumber in pokemons.results) {
+    const pokeDiv$$ = document.createElement("div")
+	const pokeImg$$ = document.createElement("img");
 
-        pokemonNumber = Number(pokemonNumber) + 1;
-        const pokeDiv$$ = document.createElement("div")
-		const pokeImg$$ = document.createElement("img");
+    pokeDiv$$.appendChild(pokeImg$$)
+    pokeDiv$$.className = "pokeCard"
 
-        pokeDiv$$.appendChild(pokeImg$$)
+    pokeImg$$.src = pokeData.sprites.front_default
+    document.body.appendChild(pokeDiv$$)
 
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNumber)
-        .then((rawPokeData) =>rawPokeData.json())
-        .then((pokeData) => {
-
-            pokeImg$$.src = pokeData.sprites.front_default
-            document.body.appendChild(pokeDiv$$)
-
-        })
-    }
 }
 
-fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151")
-	.then((data) => data.json())
-	.then((pokemons) => print(pokemons) /* console.log(pokemons) */);
+for (let pokeNumber = 1; pokeNumber <= 151; pokeNumber++) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokeNumber)
+        .then((data) => data.json())
+        .then((pokemon) => print(pokemon))
+}
+
+        
