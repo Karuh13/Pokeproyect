458d448b809bf4818be8ca9e745eca7d8bd9c257
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-14 15:55:53 +0200

    FEAT: added an h3 with the pokemon name to each pokemon

--- pokeproyect.js
+++ pokeproyect.js
@@ -3,16 +3,19 @@ const print = (pokeData) => {
     const pokeDiv$$ = document.createElement("div");
 	const pokeImg$$ = document.createElement("img");
     const pokeGif$$ = document.createElement("img");
+    const pokeName$$ = document.createElement("h3");
 
     pokeDiv$$.appendChild(pokeImg$$);
+    pokeDiv$$.appendChild(pokeName$$);
     pokeDiv$$.appendChild(pokeGif$$);
-
+    
     pokeDiv$$.className = "pokeCard";
-
+    
     pokeImg$$.src = pokeData.sprites.front_default;
     pokeGif$$.src = pokeData.sprites.versions["generation-v"]["black-white"].animated.front_default;
-    
     pokeGif$$.width *= 0.5;
+    pokeName$$.innerText = pokeData.name;
+    
     document.body.appendChild(pokeDiv$$);
 
 }
