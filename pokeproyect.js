0092c3e6dfe9b0c20db93155af0d15aa66cc80fa
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-16 18:37:30 +0200

    FEAT: the types of each pokemon

--- pokeproyect.js
+++ pokeproyect.js
@@ -1,13 +1,25 @@
 const printPokemon = (pokeData) => {
-
+    
     const pokeDiv$$ = document.createElement("div");
-	const pokeImg$$ = document.createElement("img");
-    const pokeGif$$ = document.createElement("img");
-    const pokeName$$ = document.createElement("h3");
-
+	
+    const pokeImg$$ = document.createElement("img");
     pokeDiv$$.appendChild(pokeImg$$);
+    
+    const pokeName$$ = document.createElement("h3");
     pokeDiv$$.appendChild(pokeName$$);
+    
+    const typeBox$$ = document.createElement("div");
+    for (typing of pokeData.types){
+        const pokeType$$ = document.createElement("span");
+        pokeType$$.innerText = typing.type.name;
+        pokeType$$.className = "pokeType";
+        typeBox$$.appendChild(pokeType$$);
+    }
+    pokeDiv$$.appendChild(typeBox$$);
+    
+    const pokeGif$$ = document.createElement("img");
     pokeDiv$$.appendChild(pokeGif$$);
+
     
     pokeDiv$$.className = "pokeCard";
     
