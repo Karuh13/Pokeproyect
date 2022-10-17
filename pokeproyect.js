f3f958555249aca496dccfe0d638ddad482d79be
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-17 18:34:55 +0200

    REFACTOR: clarity in code and comments for testing

--- pokeproyect.js
+++ pokeproyect.js
@@ -1,4 +1,4 @@
-const pokedex =[];
+let pokedex = [];
 
 const initializePokedex = () => {
     for (let pokeNumber = 1; pokeNumber <= 151; pokeNumber++) {
@@ -8,57 +8,75 @@ const initializePokedex = () => {
     }
 }
 
+/* const initializePokedex = () => {
+	return new Promise((resolve, reject) => {
+		const requests = [];
+		for (let pokeNumber = 1; pokeNumber <= 151; pokeNumber++) {
+			const req = fetch("https://pokeapi.co/api/v2/pokemon/" + pokeNumber)
+				.then((data) => data.json())
+				.then((pokemon) => pokemon)
+				.catch((error) => reject(error));
+			requests.push(req);
+		}
+		resolve(requests);
+	});
+}; */
+
 const printPokemon = (pokedex) => {
-    const main$$ = document.querySelector("main")
-    main$$.innerHTML = ""
+	const main$$ = document.querySelector("main");
+	main$$.innerHTML = "";
 
-    for (const pokeData of pokedex) {
-        const pokeDiv$$ = document.createElement("div");
-        
-        const pokeName$$ = document.createElement("h3");
-        pokeDiv$$.appendChild(pokeName$$);
+	for (const pokeData of pokedex) {
+		// Card
+		const pokeDiv$$ = document.createElement("div");
+		pokeDiv$$.className = "pokeCard";
 
-        const pokeImg$$ = document.createElement("img");
-        pokeDiv$$.appendChild(pokeImg$$);
-        
-        const typeBox$$ = document.createElement("div");
-        typeBox$$.className = "pokeBox";
-        for (typing of pokeData.types){
-            const pokeType$$ = document.createElement("span");
-            pokeType$$.innerText = typing.type.name.toUpperCase();
-            pokeType$$.className = typing.type.name;
-            typeBox$$.appendChild(pokeType$$);
-        }
-        pokeDiv$$.appendChild(typeBox$$);
-        
-        /* const pokeGif$$ = document.createElement("img");
+		// Name of the pokemon
+		const pokeName$$ = document.createElement("h3");
+		pokeName$$.innerHTML = pokeData.name;
+		pokeDiv$$.appendChild(pokeName$$);
+
+		// Sprite
+		const pokeImg$$ = document.createElement("img");
+		pokeImg$$.src = pokeData.sprites.front_default;
+		pokeDiv$$.appendChild(pokeImg$$);
+
+		// Types
+		const typeBox$$ = document.createElement("div");
+		typeBox$$.className = "pokeBox";
+		for (typing of pokeData.types) {
+			const pokeType$$ = document.createElement("span");
+			pokeType$$.innerHTML = typing.type.name.toUpperCase();
+			pokeType$$.className = typing.type.name;
+			typeBox$$.appendChild(pokeType$$);
+		}
+		pokeDiv$$.appendChild(typeBox$$);
+
+		/* const pokeGif$$ = document.createElement("img");
         pokeDiv$$.appendChild(pokeGif$$);
         pokeGif$$.src = pokeData.sprites.versions["generation-v"]["black-white"].animated.front_default;
         pokeGif$$.width *= 0.5; */
-    
-        
-        pokeDiv$$.className = "pokeCard";
-        
-        pokeImg$$.src = pokeData.sprites.front_default;
-        pokeName$$.innerText = pokeData.name
-        
-        main$$.appendChild(pokeDiv$$);
-    }
 
-}
+		main$$.appendChild(pokeDiv$$);
+	}
+};
 
 const searchPokemon = (input) => {
-    printPokemon(pokedex.filter((pokemon) => pokemon.name.includes(input.value) || String(pokemon.id).includes(input.value)));
-}
+	printPokemon(
+		pokedex.filter(
+			(pokemon) => pokemon.name.includes(input.value) || String(pokemon.id).includes(input.value)
+		)
+	);
+};
 
 /* Need to convert this into an async function */
 const director = () => {
-    initializePokedex();
+	initializePokedex();
 
-    setTimeout(() => printPokemon(pokedex), 1000);
+	setTimeout(() => printPokemon(pokedex), 1000);
 
-    const inputSearch$$ = document.querySelector("input");
-    inputSearch$$.addEventListener("keyup", () => searchPokemon(inputSearch$$))
-}
+	const inputSearch$$ = document.querySelector("input");
+	inputSearch$$.addEventListener("keyup", () => searchPokemon(inputSearch$$));
+};
 
-director()
+director();
