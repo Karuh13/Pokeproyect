eab82ff53bcec65e5020e2afec8f217bdf7af096
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-17 09:48:53 +0200

    REFACTOR: changed the logics of the fetching and printing functions

--- pokeproyect.js
+++ pokeproyect.js
@@ -1,42 +1,53 @@
-const printPokemon = (pokeData) => {
-    
-    const pokeDiv$$ = document.createElement("div");
-	
-    const pokeImg$$ = document.createElement("img");
-    pokeDiv$$.appendChild(pokeImg$$);
-    
-    const pokeName$$ = document.createElement("h3");
-    pokeDiv$$.appendChild(pokeName$$);
-    
-    const typeBox$$ = document.createElement("div");
-    typeBox$$.className = "pokeBox";
-    for (typing of pokeData.types){
-        const pokeType$$ = document.createElement("span");
-        pokeType$$.innerText = typing.type.name.toUpperCase();
-        pokeType$$.className = typing.type.name;
-        typeBox$$.appendChild(pokeType$$);
-    }
-    pokeDiv$$.appendChild(typeBox$$);
-    
-    const pokeGif$$ = document.createElement("img");
-    pokeDiv$$.appendChild(pokeGif$$);
+const pokedex =[];
 
-    
-    pokeDiv$$.className = "pokeCard";
-    
-    pokeImg$$.src = pokeData.sprites.front_default;
-    pokeGif$$.src = pokeData.sprites.versions["generation-v"]["black-white"].animated.front_default;
-    pokeGif$$.width *= 0.5;
-    pokeName$$.innerText = pokeData.name[0].toUpperCase() + pokeData.name.slice(1);
-    
-    document.body.appendChild(pokeDiv$$);
-
-}
-
-for (let pokeNumber = 1; pokeNumber <= 151; pokeNumber++) {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + pokeNumber)
+const initializePokedex = () => {
+    for (let pokeNumber = 1; pokeNumber <= 151; pokeNumber++) {
+        fetch("https://pokeapi.co/api/v2/pokemon/" + pokeNumber)
         .then((data) => data.json())
-        .then((pokemon) => printPokemon(pokemon))
+        .then((pokemon) => pokedex.push(pokemon))
+    }
 }
 
+const printPokemon = (pokedex) => {
+    for (const pokeData of pokedex) {
+        const pokeDiv$$ = document.createElement("div");
         
+        const pokeImg$$ = document.createElement("img");
+        pokeDiv$$.appendChild(pokeImg$$);
+        
+        const pokeName$$ = document.createElement("h3");
+        pokeDiv$$.appendChild(pokeName$$);
+        
+        const typeBox$$ = document.createElement("div");
+        typeBox$$.className = "pokeBox";
+        for (typing of pokeData.types){
+            const pokeType$$ = document.createElement("span");
+            pokeType$$.innerText = typing.type.name.toUpperCase();
+            pokeType$$.className = typing.type.name;
+            typeBox$$.appendChild(pokeType$$);
+        }
+        pokeDiv$$.appendChild(typeBox$$);
+        
+        const pokeGif$$ = document.createElement("img");
+        pokeDiv$$.appendChild(pokeGif$$);
+    
+        
+        pokeDiv$$.className = "pokeCard";
+        
+        pokeImg$$.src = pokeData.sprites.front_default;
+        pokeGif$$.src = pokeData.sprites.versions["generation-v"]["black-white"].animated.front_default;
+        pokeGif$$.width *= 0.5;
+        pokeName$$.innerText = pokeData.name[0].toUpperCase() + pokeData.name.slice(1);
+        
+        document.body.appendChild(pokeDiv$$);
+    }
+
+}
+/* Need to convert this into an async function */
+function director () {
+    initializePokedex();
+
+    setTimeout(() => printPokemon(pokedex), 1000);
+}
+
+director()
