351c76e37bffbd4813317220c93406db0f8b2d79
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-18 13:00:39 +0200

    FEAT: stylization to the detailed box, not finished

--- pokeproyect.js
+++ pokeproyect.js
@@ -8,10 +8,16 @@ const initializePokedex = async (pokemonLimit = 151) => {
             const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + index);
             const result = await response.json();
             pokedex.push(result);
-			
+
 			const specieFetch = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + index);
-			const specieInfo = await specieFetch.json();
-			pokedex[index - 1].description = specieInfo.flavor_text_entries[17].flavor_text;
+			let specieInfo = await specieFetch.json();
+			for (quote of specieInfo.flavor_text_entries) {
+				if (quote.language.name === "en"){
+					specieInfo = quote.flavor_text;
+					break
+				}
+			}
+			pokedex[index - 1].description = specieInfo.split("\f").join(" ");
         }
     } catch (error) {
         console.log(error);
@@ -49,7 +55,7 @@ const printPokemon = (pokedex) => {
 		}
 		pokeDiv$$.appendChild(typeBox$$);
 
-		pokeDiv$$.addEventListener("click", () => detailedView(pokeData, main$$))
+		pokeDiv$$.addEventListener("click", () => detailedView(pokeData, main$$, pokeDiv$$))
 
 		main$$.appendChild(pokeDiv$$);
 	}
@@ -63,31 +69,33 @@ const searchPokemon = (input) => {
 	);
 };
 
-const detailedView = async (pokeData, main$$) => {
-	main$$.innerHTML = "";
+const detailedView = async (pokeData, main$$, pokeDiv$$) => {
+	pokeDiv$$.innerHTML = "";
 	
 	// Card with details
-	const detailsCard$$ = document.createElement("div")
-	detailsCard$$.className = "pokeDetails"
+	/* const pokeDiv$$ = document.createElement("div") */
+	pokeDiv$$.className = "pokeDetails"
 
 	// Name of the pokemon
 	const pokeName$$ = document.createElement("h3");
 	pokeName$$.innerHTML = pokeData.name;
-	detailsCard$$.appendChild(pokeName$$);
+	pokeDiv$$.appendChild(pokeName$$);
 
 	// Closing button
 	const closingButton$$ = document.createElement("button")
-	detailsCard$$.appendChild(closingButton$$)
+	pokeDiv$$.appendChild(closingButton$$)
 	closingButton$$.innerHTML = "X"
 	closingButton$$.className = "closing-button"
 	closingButton$$.addEventListener("click", () => searchPokemon(document.querySelector("input")))
 
-	// Div for animations, stats and description
+	// Div for animations and stats + description
 	const informationDiv$$ = document.createElement("div")
-	detailsCard$$.appendChild(informationDiv$$)
+	informationDiv$$.className= "information"
+	pokeDiv$$.appendChild(informationDiv$$)
 
 	// Div for both animations
 	const animationsDiv$$ = document.createElement("div");
+	animationsDiv$$.className = "animations"
 	informationDiv$$.appendChild(animationsDiv$$)
 
 	// Animated front view of the pokemon 
@@ -102,35 +110,40 @@ const detailedView = async (pokeData, main$$) => {
     pokeGifBack$$.src = pokeData.sprites.versions["generation-v"]["black-white"].animated.back_default;
 	pokeGifBack$$.title = pokeData.name[0].toUpperCase() + pokeData.name.slice(1)
 
+	// Div for stats + description
+	const statAndDescriptionDiv$$ = document.createElement("div")
+	statAndDescriptionDiv$$.className = "stat-description"
+	informationDiv$$.appendChild(statAndDescriptionDiv$$)
+
 	// Div for stats
 	const statBox$$ = document.createElement("div")
-	informationDiv$$.appendChild(statBox$$)
+	statBox$$.className = "stats-box"
+	statAndDescriptionDiv$$.appendChild(statBox$$)
+
 
 	// Stats
 	for (statData of pokeData.stats){
-		const stat$$ = document.createElement("span")
-		stat$$.innerHTML = statData.stat.name + statData.base_stat
+		const stat$$ = document.createElement("div")
+		stat$$.innerHTML = `<span>${statData.stat.name}</span><span>----</span><span>${statData.base_stat}</span>`
 		statBox$$.appendChild(stat$$)
 	}
 
 	// Description
-	const descriptionDiv$$ = document.createElement("div")
-	descriptionDiv$$.innerHTML = pokeData.description
-	informationDiv$$.appendChild(descriptionDiv$$)
+	const descriptionP$$ = document.createElement("p")
+	descriptionP$$.innerHTML = pokeData.description
+	descriptionP$$.className = "description"
+	statAndDescriptionDiv$$.appendChild(descriptionP$$)
 
 	// Types
 	const typeBox$$ = document.createElement("div");
-	typeBox$$.className = "pokeBox-details";
+	typeBox$$.className = "pokeBox";
 	for (typing of pokeData.types) {
 		const pokeType$$ = document.createElement("span");
 		pokeType$$.innerHTML = typing.type.name.toUpperCase();
 		pokeType$$.className = typing.type.name;
 		typeBox$$.appendChild(pokeType$$);
 	}
-	detailsCard$$.appendChild(typeBox$$);
-	
-	
-	main$$.appendChild(detailsCard$$)
+	pokeDiv$$.appendChild(typeBox$$);
 }
 
 
